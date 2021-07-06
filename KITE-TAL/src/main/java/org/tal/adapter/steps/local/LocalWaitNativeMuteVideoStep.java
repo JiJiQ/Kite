88ba092150ec32f522039aa319e737a399cf6d06
jijunqing <jijunqing@tal.com>
2021-07-06 15:12:26 +0800

    update

---
+++ KITE-TAL/src/main/java/org/tal/adapter/steps/local/LocalWaitNativeMuteVideoStep.java
@@ -0,0 +1,34 @@
+package org.tal.adapter.steps.local;
+
+import io.cosmosoftware.kite.exception.KiteTestException;
+import io.cosmosoftware.kite.steps.TestStep;
+import io.cosmosoftware.kite.util.TestUtils;
+import org.tal.adapter.pages.local.LocalO2oRTCPage;
+import org.webrtc.kite.tests.TestRunner;
+
+public class LocalWaitNativeMuteVideoStep extends TestStep {
+    LocalO2oRTCPage localO2oRTCPage;
+
+    public LocalWaitNativeMuteVideoStep(TestRunner runner){
+        super(runner);
+        this.localO2oRTCPage=new LocalO2oRTCPage(runner);
+    }
+    @Override
+    protected void step() throws KiteTestException {
+        String remoteVideoEnabled="uninit";
+        while (true){
+            remoteVideoEnabled=localO2oRTCPage.getVideoState("1001");
+            if("false".equalsIgnoreCase(remoteVideoEnabled)){
+                break;
+            }else{
+                logger.info("remote 还未操作mute视频，等待2s。remoteVideoEnabled："+remoteVideoEnabled);
+                TestUtils.waitAround(2000);
+            }
+        }
+    }
+
+    @Override
+    public String stepDescription() {
+        return "半自动化：等待native mute视频";
+    }
+}
---
+++ KITE-TAL/src/main/java/org/tal/adapter/tests/WebRtcNativeVideoControlTest.java
@@ -0,0 +1,53 @@
+package org.tal.adapter.tests;
+
+import io.cosmosoftware.kite.steps.WebRTCInternalsStep;
+import io.cosmosoftware.kite.util.WebDriverUtils;
+import org.tal.adapter.checks.local.LocalPeerConnectionCheck;
+import org.tal.adapter.checks.local.LocalSubscribeVideoDisplayCheck;
+import org.tal.adapter.checks.local.LocalSubscribeVideoMutedCheck;
+import org.tal.adapter.checks.local.LocalSubscribeVideoUnmutedCheck;
+import org.tal.adapter.steps.ScreenRecordStep;
+import org.tal.adapter.steps.local.LocalJoinRoomWaitStep;
+import org.tal.adapter.steps.local.LocalWaitNativeMuteVideoStep;
+import org.tal.adapter.steps.local.LocalWaitNativeUnMuteVideoStep;
+import org.webrtc.kite.tests.TestRunner;
+
+
+public class WebRtcNativeVideoControlTest extends TalTest {
+
+    @Override
+    protected void populateTestSteps(TestRunner runner) {
+
+        LocalJoinRoomWaitStep localJoinRoomWaitStep =
+                new LocalJoinRoomWaitStep(runner);
+        LocalWaitNativeMuteVideoStep localWaitNativeMuteVideoStep =
+                new LocalWaitNativeMuteVideoStep(runner);
+        LocalWaitNativeUnMuteVideoStep localWaitNativeUnMuteVideoStep =
+                new LocalWaitNativeUnMuteVideoStep(runner);
+
+        //local先进，remote后进，否则会导致streamMap混乱
+
+        localJoinRoomWaitStep.setRoomId(roomId);
+        localJoinRoomWaitStep.setUserId(localUserId);
+        localJoinRoomWaitStep.setRemoteUserId(rtnUserId);
+        runner.addStep(localJoinRoomWaitStep);
+
+        runner.addStep(new LocalPeerConnectionCheck(runner));
+        runner.addStep(new LocalSubscribeVideoDisplayCheck(runner,rtnUserId));
+
+        runner.addStep(localWaitNativeMuteVideoStep);
+
+        runner.addStep(new LocalSubscribeVideoMutedCheck(runner,roomId));
+        runner.addStep(new ScreenRecordStep(runner,"remote操作了mute audio，demo截图"));
+        runner.addStep(localWaitNativeUnMuteVideoStep);
+
+        runner.addStep(new LocalSubscribeVideoUnmutedCheck(runner,roomId));
+        runner.addStep(new ScreenRecordStep(runner,"remote操作了unmute audio，demo截图"));
+
+        if (WebDriverUtils.isChrome(runner.getWebDriver())) {
+            runner.addStep(new WebRTCInternalsStep(runner));
+        }
+
+        runner.addStep(new ScreenRecordStep(runner,"测试应该通过了，demo截图。"));
+    }
+}
