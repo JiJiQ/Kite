de6e98ecee0c1bb9cbba2fffc16a349cce3fd5ea
jijunqing <jijunqing@tal.com>
2021-07-13 10:46:02 +0800

    update

---
+++ KITE-TAL/src/main/java/org/tal/adapter/checks/local/LocalSubscribeVideoMutedRejoinCheck.java
@@ -0,0 +1,56 @@
+package org.tal.adapter.checks.local;
+
+import io.cosmosoftware.kite.exception.KiteTestException;
+import io.cosmosoftware.kite.interfaces.Runner;
+import io.cosmosoftware.kite.report.Status;
+import io.cosmosoftware.kite.steps.TestCheck;
+import io.cosmosoftware.kite.util.TestUtils;
+import org.tal.adapter.pages.local.LocalO2oRTCPage;
+
+import static io.cosmosoftware.kite.util.ReportUtils.saveScreenshotPNG;
+
+public class LocalSubscribeVideoMutedRejoinCheck extends TestCheck {
+    protected LocalO2oRTCPage localO2oRTCPage;
+    String remoteId;
+    public LocalSubscribeVideoMutedRejoinCheck(Runner runner, String remoteId) {
+        super(runner);
+        localO2oRTCPage = new LocalO2oRTCPage(runner);
+        this.remoteId=remoteId;
+    }
+
+    @Override
+    public String stepDescription() {
+        return "退出重进后验证拉流视频的muted是否为true";
+    }
+
+    @Override
+    protected void step() {
+        try {
+            String VideoMuted = "uninit";
+            for (int elapsedTime = 0; elapsedTime < this.checkTimeout; elapsedTime += this.checkInterval) {
+                logger.info("获取订阅流视频状态和muted");
+                VideoMuted = localO2oRTCPage.getVideoState(remoteId);
+                if (!"true".equalsIgnoreCase(VideoMuted)&&!"unknow".equalsIgnoreCase(VideoMuted)) {
+                    logger.error("订阅视频muted为："+VideoMuted);
+                    TestUtils.waitAround(this.checkInterval);
+                } else {
+                    logger.info("订阅流视频muted为：" + VideoMuted);
+                    reporter.textAttachment(report, "订阅视频muted为：",VideoMuted, "plain");
+                    return;
+                }
+            }
+            if(!"true".equalsIgnoreCase(VideoMuted)){
+                throw new KiteTestException("订阅视频muted为："+VideoMuted,Status.FAILED);
+            }
+        } catch (Exception e) {
+            //force silent to false in case of error, so the failure appears in the report in all cases.
+                try {
+                    String screenshotName = "error_screenshot_" + this.getName();
+                    reporter.screenshotAttachment(this.report, screenshotName, saveScreenshotPNG(webDriver));
+                } catch (KiteTestException ex) {
+                    logger.warn("Could not attach screenshot to error of step: " + stepDescription());
+                }
+            reporter.processException(this.report, e, true);
+        }
+    }
+}
